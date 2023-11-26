334c4075d6a4e27a079b6879e3a41e3f972bdc7e
FNaotoUSP <fabio.naoto.mh@usp.br>
2023-11-26 20:14:09 -0300

    Arquivo branch2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# branch2
 import hashlib
 
 def hash_password(password):
@@ -5,9 +6,14 @@ def hash_password(password):
     hashed_password = hash_object.hexdigest()
     return hashed_password
 
-def save_to_file(data, filename):
-    with open(filename, 'w') as file:
-        file.write(data)
+def check_password(file_path, hashed_password_to_check):
+    with open(file_path, 'r') as file:
+        stored_hashed_password = file.read().strip()
+
+    if hashed_password_to_check == stored_hashed_password:
+        print("A senha está correta.")
+    else:
+        print("A senha está incorreta.")
 
 if __name__ == "__main__":
     with open('senha.txt', 'r') as file:
@@ -15,4 +21,4 @@ if __name__ == "__main__":
 
     hashed_password = hash_password(password)
 
-    save_to_file(hashed_password, 'hashed_password.txt')
+    check_password('hashed_password.txt', hashed_password)
